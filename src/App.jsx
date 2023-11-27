28edb91eb4ce82f6039cf9194de7e53c4546aea1
yavtuh <yavtushenko2018seriy@gmail.com>
2023-11-27 17:40:34 +0200

    dz4

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,52 @@
-import Counter from "./components/Counter/Counter"
+import { useEffect, useState } from "react"
+import User from "./components/User/User"
+
 
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        setUsers(data);
+      }catch(err){
+        console.log(err);
+      } finally{
+        setIsLoading(false);
+      }
+    }
+
+    getUsers();
+
+    const handleScroll = () => {
+      console.log('Scroll');
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+
+  }, []);
   
+  if (isLoading) {
+    return (
+      <>
+        Loading......
+      </>
+    );
+  }
+
   return (
     <>
-      <Counter />
+      { users.length > 0 && users.map(user =>{
+          return <User key={user.id} user={user}/>
+      })}
     </>
   )
 }
